6b538e8e05b1ba397188fb84637479c4f84382a5
Nicolas Osborne <nho.osborne@gmail.com>
2024-04-04 18:17:44 +0200

    Correction de l'affichage du dernier Event dans le Footer - Déclaration de "last"

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,9 @@ import Modal from '../../containers/Modal'
 import { useData } from '../../contexts/DataContext'
 
 const Page = () => {
-  const { last } = useData()
+  const { data } = useData()
+  const last = data ? data.events[data.events.length - 1] : null
+
   return (
     <>
       <header>
@@ -113,13 +115,13 @@ const Page = () => {
       </main>
       <footer className='row'>
         <div className='col presta'>
-          <h3>Notre derniére prestation</h3>
+          <h3>Notre dernière prestation</h3>
           <EventCard
             imageSrc={last?.cover}
             title={last?.title}
             date={new Date(last?.date)}
+            label={last?.type}
             small
-            label='boom'
           />
         </div>
         <div className='col contact'>
