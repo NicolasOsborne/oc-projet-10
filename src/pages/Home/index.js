4b3462439b00ff6e8baf81ec87515019043bb3ff
Nicolas Osborne <nho.osborne@gmail.com>
2024-04-09 10:46:48 +0200

    Correction des erreurs dans la console liées aux valeurs "undefined" des props de EventCard dans le Footer (ajout du conditionnement du rendu du composant EventCard lorsque last est défini)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -116,13 +116,15 @@ const Page = () => {
       <footer className='row'>
         <div className='col presta'>
           <h3>Notre dernière prestation</h3>
-          <EventCard
-            imageSrc={last?.cover}
-            title={last?.title}
-            date={new Date(last?.date)}
-            label={last?.type}
-            small
-          />
+          {last && (
+            <EventCard
+              imageSrc={last?.cover}
+              title={last?.title}
+              date={new Date(last?.date)}
+              label={last?.type}
+              small
+            />
+          )}
         </div>
         <div className='col contact'>
           <h3>Contactez-nous</h3>
